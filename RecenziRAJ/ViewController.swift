ef40e58e79fdada07b3acce8558f13f99a14a252
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-08-27 19:01:36 +0200

    Initial Commit

---
+++ RecenziRAJ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecenziRAJ
+//
+//  Created by Mateo on 27.08.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
