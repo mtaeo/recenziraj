cad3fea89c33af4d0775d9e3c57fe546691652cc
Mateo Aladić <87236729+mtaeo@users.noreply.github.com>
2022-10-25 15:45:15 +0200

    Implement basic image classfying

--- RecenziRAJ/Presentation/MainTabBar/Home/HomeViewController.swift
+++ RecenziRAJ/Presentation/MainTabBar/Home/HomeViewController.swift
@@ -8,6 +8,40 @@
 import UIKit
 
 final class HomeViewController: BaseViewController<HomeViewModel> {
+    
+    private lazy var imagePicker: ImagePicker = {
+        let imagePicker = ImagePicker(presentationController: self, delegate: self)
+        return imagePicker
+    }()
+    
+    private lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private lazy var textView: UILabel = {
+        let textView = UILabel()
+        textView.text = "TextView test"
+        textView.backgroundColor = .red
+        return textView
+    }()
+    
+    private lazy var takeImageButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "camera.fill"), for: .normal)
+        button.addTarget(self, action: #selector(takeImageButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +58,49 @@ private extension HomeViewController {
     }
     
     func addSubviews() {
-        
+        containerStackView.addArrangedSubview(imageView)
+        containerStackView.addArrangedSubview(textView)
+        containerStackView.addArrangedSubview(takeImageButton)
+        view.addSubview(containerStackView)
     }
     
     func makeConstraints() {
+        containerStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(300)
+        }
+        
+        textView.snp.makeConstraints {
+            $0.height.equalTo(30)
+            $0.leading.trailing.equalToSuperview()
+        }
         
     }
+    
+    @objc func takeImageButtonTapped(_ sender: UIButton) {
+        imagePicker.present(from: sender)
+    }
+}
+
+extension HomeViewController: ImagePickerDelegate {
+    func didSelect(image: UIImage?) {
+        imageView.image = image
+        
+        if let image = image {
+            viewModel.updateClassifications(for: image) { [weak self] classifications in
+                if let classifications = classifications {
+                    self?.viewModel.nicePrint(classifications)
+                    self?.textView.text = "\(classifications.first?.identifier ?? "") ---> \(classifications.first?.confidence.rounded())"
+                } else {
+                    print("got nothing")
+                }
+
+            } errorHandler: { error in
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+
+    
 }
--- RecenziRAJ/Presentation/MainTabBar/Home/HomeViewModel.swift
+++ RecenziRAJ/Presentation/MainTabBar/Home/HomeViewModel.swift
@@ -5,12 +5,61 @@
 //  Created by Mateo on 08.09.2022..
 //
 
-import Foundation
+import CoreML
+import Vision
+import UIKit
 
 final class HomeViewModel: BaseViewModel {
     private let authService: AuthService
     
+    private lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel.init(for: RecenzirajClassifier.init(configuration: MLModelConfiguration()).model)
+            let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            }
+            
+            return request
+        } catch {
+            fatalError("Failed to load ML Model: \(error)")
+        }
+    }()
+
+    private var onClassificationSucceeded: (([VNClassificationObservation]?) -> Void)?
+    
     init(authService: AuthService) {
         self.authService = authService
+    }
+}
+
+extension HomeViewModel {
+    func updateClassifications(for image: UIImage, completionHandler: @escaping ([VNClassificationObservation]?) -> Void, errorHandler: (Error) -> Void) {        
+        let ciImage = CIImage(image: image)!
+        let handler = VNImageRequestHandler(ciImage: ciImage, orientation: .up)
+        
+        do {
+            onClassificationSucceeded = completionHandler
+            try handler.perform([classificationRequest])
+        } catch {
+            print("Failed to perform classifications. \n \(error.localizedDescription)")
+            errorHandler(error)
+        }
+    }
+    
+    func nicePrint(_ classifications: [VNClassificationObservation]) {
+        print("\n\n")
+        classifications.forEach { e in
+            print("\(e.identifier) + \(e.confidence)")
+        }
+    }
+}
+
+private extension HomeViewModel {
+    func processClassifications(for request: VNRequest, error: Error?) {
+        let classifications = request.results as! [VNClassificationObservation]
+                        
+        classifications.isEmpty
+            ? onClassificationSucceeded?(nil)
+            : onClassificationSucceeded?(classifications)
     }
 }
